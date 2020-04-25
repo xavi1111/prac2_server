eca8a1ca8fbb562dbf277b8c6d5fe5e537a2a7e5
xavier <xavierllubes11@gmail.com>
2020-04-25 19:27:24 +0200

    Inicialitzacio de les calsses de Nivell i TipoLocal

--- prac2_serverWeb/src/pojo/Nivell.java
+++ prac2_serverWeb/src/pojo/Nivell.java
@@ -1,5 +1,83 @@
 package pojo;
 
-public class Nivell {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class Nivell {
+	private Long codiNivell;
+	private String nomNivellCA;
+	private String nomNivellES;
+	private String nomNivellEN;
+	private Connection dbConnection;
+	
+	public Nivell(Connection dbConnection) {
+		this.dbConnection = dbConnection;
+	}
+	
+	public Boolean LoadItem(Long codiNivell) {
+		ResultSet ors = null;
+		try {
+			Statement statement = dbConnection.createStatement();
+			ors = statement.executeQuery("select * from Nivell where Nivell.codiNivell = '" + codiNivell + "'");
+			if(ors.next()) {
+				fillObject(ors);
+				ors.close();
+				return true;
+			}else
+				return false;
+		}catch(Exception e) {
+			//TODO a veure que fem
+			try {
+				ors.close();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return false;
+		}
+	}
+
+	private void fillObject(ResultSet ors) {
+		try {
+			this.codiNivell = ors.getLong("codiNivell");
+			this.nomNivellCA = ors.getString("nomNivellCA");
+			this.nomNivellES = ors.getString("nomNivellES");
+			this.nomNivellEN = ors.getString("nomNivellEN");
+		}catch(Exception e) {
+			//TODO a veure que fem
+		}
+	}
+
+	public Long getCodiNivell() {
+		return codiNivell;
+	}
+
+	public void setCodiNivell(Long codiNivell) {
+		this.codiNivell = codiNivell;
+	}
+
+	public String getNomiNivellCA() {
+		return nomNivellCA;
+	}
+
+	public void setNomNivellCA(String nomNivellCA) {
+		this.nomNivellCA = nomNivellCA;
+	}
+
+	public String getCodiNivellES() {
+		return nomNivellES;
+	}
+
+	public void setNomNivellES(String nomNivellES) {
+		this.nomNivellES = nomNivellES;
+	}
+
+	public String getCodiNivellEN() {
+		return nomNivellEN;
+	}
+
+	public void setNomNivellEN(String nomNivellEN) {
+		this.nomNivellEN = nomNivellEN;
+	}
 }
--- prac2_serverWeb/src/pojo/TipoLocal.java
+++ prac2_serverWeb/src/pojo/TipoLocal.java
@@ -1,5 +1,82 @@
 package pojo;
 
-public class TipoLocal {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class TipoLocal {
+	private Long codiTipoLocal;
+	private String nomTipoLocalCA;
+	private String nomTipoLocalES;
+	private String nomTipoLocalEN;
+	private Connection dbConnection;
+
+	public TipoLocal(Connection dbConnection) {
+		this.dbConnection = dbConnection;
+	}	
+	
+	public Boolean LoadItem(Long codiTipoLocal) {
+		ResultSet ors = null;
+		try {
+			Statement statement = dbConnection.createStatement();
+			ors = statement.executeQuery("select * from TipoLocal where TipoLocal.codiTipoLocal = '" + codiTipoLocal + "'");
+			if(ors.next()) {
+				fillObject(ors);
+				ors.close();
+				return true;
+			}else
+				return false;
+		}catch(Exception e) {
+			try {
+				ors.close();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return false;
+		}
+	}
+
+	private void fillObject(ResultSet ors) {
+		try {
+			this.codiTipoLocal = ors.getLong("codiTipoLocal");
+			this.nomTipoLocalCA = ors.getString("nomTipoLocalCA");
+			this.nomTipoLocalES = ors.getString("nomTipoLocalES");
+			this.nomTipoLocalEN = ors.getString("nomTipoLocalEN");
+		}catch(Exception e) {
+			//TODO a veure que fem
+		}
+	}
+
+	public Long getCodiTipoLocal() {
+		return codiTipoLocal;
+	}
+
+	public void setCodiTipoLocal(Long codiTipoLocal) {
+		this.codiTipoLocal = codiTipoLocal;
+	}
+
+	public String getNomTipoLocalCA() {
+		return nomTipoLocalCA;
+	}
+
+	public void setNomTipoLocalCA(String nomTipoLocalCA) {
+		this.nomTipoLocalCA = nomTipoLocalCA;
+	}
+
+	public String getNomTipoLocalES() {
+		return nomTipoLocalES;
+	}
+
+	public void setNomTipoLocalES(String nomTipoLocalES) {
+		this.nomTipoLocalES = nomTipoLocalES;
+	}
+
+	public String getNomTipoLocalEN() {
+		return nomTipoLocalEN;
+	}
+
+	public void setNomTipoLocalEN(String nomTipoLocalEN) {
+		this.nomTipoLocalEN = nomTipoLocalEN;
+	}
 }
